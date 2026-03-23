c5f730e5a46f745bba474ae366b70fe2a87ef688
Jay Ramani <jayramani.dev@gmail.com>
2026-03-23 02:16:40 -0400

    fix: resolve Vercel 404, add scroll to top, update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import ProjectDetails from './pages/ProjectDetails'
 import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
 import ProtectedRoute from './components/ProtectedRoute'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
