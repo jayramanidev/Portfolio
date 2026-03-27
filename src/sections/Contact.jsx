a37d3c671052f8e977599b0c741b24ae0a320bd9
Jay Ramani <jayramani.dev@gmail.com>
2026-03-27 04:32:23 -0400

    Fix mobile navbar, update contact email, update eslint config

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -68,7 +68,7 @@ function Contact() {
             >
                 <div className="contact-links">
                     <a
-                        href="mailto:jay@ramanidev.com"
+                        href="mailto:jayramani.dev@gmail.com"
                         className="contact-link"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -77,7 +77,7 @@ function Contact() {
                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
                             <polyline points="22,6 12,13 2,6" />
                         </svg>
-                        <span className="contact-link-text">jay@ramanidev.com</span>
+                        <span className="contact-link-text">jayramani.dev@gmail.com</span>
                     </a>
 
                     <a
